0edff5f7594b0133f43bd71285f0971510a07114
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-04-04 22:28:02 -0300

    .

--- src/components/BackgrooundAnimation/BackgroundAnimation.js
+++ src/components/BackgrooundAnimation/BackgroundAnimation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const BackgroundAnimation = () => (
-  <div style={{ width: '40vw', maxWidth: '50%' }}>
+  <div style={{ width: '30vw', maxWidth: '40%' }}>
     <svg
       className='BgAnimation__svg'
       viewBox='0 0 602 602'
